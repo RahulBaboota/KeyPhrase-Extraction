2d56b73fcb18e8eed1267ff10eaed6c7ef760d17
RahulBaboota <rahulbaboota08@gmail.com>
2016-09-27 17:26:06 +0530

    Added code for computing matrix without normalisation and for basic tf case

--- KeyWordExtractor/TF-IDF.py
+++ KeyWordExtractor/TF-IDF.py
@@ -14,6 +14,7 @@ from collections import OrderedDict
 from TextPreProcessing import TextTokenize
 from TextPreProcessing import Extract_Candidates_Nouns
 from TextPreProcessing import Extract_Candidates_Nouns_Adjective
+import numpy as np
 
 ## Defining Toy Text
 Text = ['Rashid Siddiqui kept hearing those words from his fellow Muslim pilgrims lying mangled on the ground in 118-degree heat, under a searing Saudi sun. Barefoot, topless and dazed, Mr. Siddiqui had somehow escaped being crushed by the surging crowd.It was Sept. 24, 2015, the third morning of the hajj, the annual five-day pilgrimage to Mecca by millions of Muslims from around the world. By some estimates, it was the deadliest day in hajj history and one of the worst accidents in the world in decades.An American from Atlanta, Mr. Siddiqui, 42, had been walking through a sprawling valley of tens of thousands of pilgrim tents. His destination: Jamarat Bridge, where pilgrims throw pebbles at three large pillars in a ritual symbolizing the stoning of the devil. He was less than a mile from the bridge when the crush began.']
@@ -40,27 +41,27 @@ def TF_IDF_Candidates_All(Text):
     # Creating the vector in our Vector Space Model with the help of the above created dictionary .
     # It is important to note that herein we are creating a single vector which makes records of frequency count for the tokens
     # in the entire article .
-    Word_Vectors = Count_Vectorizer.transform(Text).todense()
-    # print Word_Vectors
+    TF_Matrix = Count_Vectorizer.transform(Text).todense()
+    # print TF_Matrix.shape
 
     # Creating the vector for each document (sentence) for helping us evaluate the idf weight for each matrix .
     IDF_Help = Count_Vectorizer.transform(Sentence_Tokens).todense()
     # print IDF_Help
 
     ## Instantiating the TF-IDF Transformer to compute idf weights for each word.
-    TFIDF = TfidfTransformer(norm="l2")
+    TFIDF = TfidfTransformer()
     TFIDF.fit(IDF_Help)
     IDF_Matrix = TFIDF.idf_
+    # print IDF_Matrix
 
-    # TFIDF_matrix = TFIDF.transform(Word_Vectors).todense()
-    # print TFIDF_matrix
+    ## Converting the IDF_Matrix to a square diagonal matrix for vector multiplication with TF_Matrix Matrix to evaluate 
+    ## the resulting matrix with final scores .
+    IDF_Square_Matrix = np.diag(IDF_Matrix)
+    # print IDF_Square_Matrix
 
-
-
-
-
-    
-
-
+    ## Multiplying Matrices "TF_Matrix" and "IDF_Square_Matrix" to obtain the final matrix with final weighted scores .
+    Final_Weight_Matrix = np.dot(TF_Matrix,IDF_Square_Matrix)
+    # print Final_Weight_Matrix.shape
+    # print Final_Weight_Matrix
 
 TF_IDF_Candidates_All(Text)
