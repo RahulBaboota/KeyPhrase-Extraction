c30b4e80b781672cb6be29018e488d0c00e90fd1
RahulBaboota <rahulbaboota08@gmail.com>
2016-10-02 11:26:40 +0530

    Reset Function

--- KeyWordExtractor/TF-IDF.py
+++ KeyWordExtractor/TF-IDF.py
@@ -18,7 +18,7 @@ from TextPreProcessing import Extract_Candidates_Nouns_Adjective
 import numpy as np
 
 ## Defining Toy Text
-Text = ['Rashid Siddiqui kept hearing those words from his fellow Muslim pilgrims lying mangled on the ground in 118-degree heat, under a searing Saudi sun. Barefoot, topless and dazed, Mr. Siddiqui had somehow escaped being crushed by the surging crowd.It was Sept. 24, 2015, the third morning of the hajj, the annual five-day pilgrimage to Mecca by millions of Muslims from around the world. By some estimates, it was the deadliest day in hajj history and one of the worst accidents in the world in decades.An American from Atlanta, Mr. Siddiqui, 42, had been walking through a sprawling valley of tens of thousands of pilgrim tents. His destination: Jamarat Bridge, where pilgrims throw pebbles at three large pillars in a ritual symbolizing the stoning of the devil. He was less than a mile from the bridge when the crush began.']
+Text = ['Rashid Siddiqui kept hearing those words from his fellow Muslim pilgrims lying mangled on the ground in 118 degree heat, under a searing Saudi sun. Barefoot, topless and dazed, Mr. Siddiqui had somehow escaped being crushed by the surging crowd. It was Sept. 24, 2015, the third morning of the hajj, the annual five day pilgrimage to Mecca by millions of Muslims from around the world. By some estimates, it was the deadliest day in hajj history and one of the worst accidents in the world in decades. An American from Atlanta, Mr. Siddiqui, 42, had been walking through a sprawling valley of tens of thousands of pilgrim tents. His destination: Jamarat Bridge, where pilgrims throw pebbles at three large pillars in a ritual symbolizing the stoning of the devil. He was less than a mile from the bridge when the crush began.']
 
 ## Breaking the test into individual sentences to act as input to CountVectorizer Class .
 
@@ -131,17 +131,6 @@ def TF_IDF_Candidates_All(Text,norm,term_frequency):
 """ 
 
 def TF_IDF_Nouns(Text,norm,term_frequency):
-
-    All_Tokens_Scores = TF_IDF(Text,norm=norm,term_frequency=term_frequency)
-
-    ## Storing the Text Tokens in a list 
-    Text_Tokens = []
-
-    for key in All_Tokens_Scores:
-        Text_Tokens.append(key)
-
-    ## Extracting the candidates from the created tokens which in this case is "nouns"
-    Candidates_Nouns = Extract_Candidates_Nouns(Text_Tokens)
 
 
 
